2720151c238c632745404dbc972390e26172baa4
Riley McGee <mcgeer@mcmaster.ca>
2016-03-15 16:40:17 -0400

    Jars ReBuilt

--- src/UserInterface/SolarSubComposite.java
+++ src/UserInterface/SolarSubComposite.java
@@ -14,14 +14,13 @@ public class SolarSubComposite extends Composite {
 	private Text solarPowerEfficienyText;
 	private Text exposureText;
 	private Text[] textBoxes;
-	private RowLayout layout = new RowLayout();
 
 	public SolarSubComposite(Composite arg0, int arg1) {
 		super(arg0, arg1);
 		addElementsToComposite();
 		setBackground(ColorPalette.CUSTOM_BLACK);
 		setForeground(ColorPalette.CUSTOM_BLUE);
-		setLayout(null);
+		setLayout(new RowLayout());
 		setBounds(0, 100, 540, 200);
 	}
 
@@ -29,43 +28,43 @@ public class SolarSubComposite extends Composite {
 
 		Label lblAreaText = new Label(this, SWT.NONE);
 		lblAreaText.setFont(SWTResourceManager.getFont("Segoe UI", 10, SWT.NORMAL));
-		lblAreaText.setBounds(0, 0, 163, 35);
+		lblAreaText.setBounds(25, 0, 163, 35);
 		lblAreaText.setText("Panel Area:");
 		lblAreaText.setBackground(ColorPalette.CUSTOM_BLACK);
 		lblAreaText.setForeground(ColorPalette.CUSTOM_BLUE);
 
 		Label lblPowerloss = new Label(this, SWT.NONE);
 		lblPowerloss.setFont(SWTResourceManager.getFont("Segoe UI", 10, SWT.NORMAL));
-		lblPowerloss.setBounds(0, 40, 163, 35);
+		lblPowerloss.setBounds(25, 40, 163, 35);
 		lblPowerloss.setText("Powerloss Coefficient:");
 		lblPowerloss.setBackground(ColorPalette.CUSTOM_BLACK);
 		lblPowerloss.setForeground(ColorPalette.CUSTOM_BLUE);
 
 		Label lblPowerEfficiency = new Label(this, SWT.NONE);
 		lblPowerEfficiency.setFont(SWTResourceManager.getFont("Segoe UI", 10, SWT.NORMAL));
-		lblPowerEfficiency.setBounds(0, 80, 163, 35);
+		lblPowerEfficiency.setBounds(25, 80, 163, 35);
 		lblPowerEfficiency.setText("Panel Efficiency:");
 		lblPowerEfficiency.setBackground(ColorPalette.CUSTOM_BLACK);
 		lblPowerEfficiency.setForeground(ColorPalette.CUSTOM_BLUE);
 
 		Label lblExposure = new Label(this, SWT.NONE);
 		lblExposure.setFont(SWTResourceManager.getFont("Segoe UI", 10, SWT.NORMAL));
-		lblExposure.setBounds(0, 120, 163, 35);
+		lblExposure.setBounds(25, 120, 163, 35);
 		lblExposure.setText("Panel Exposure:");
 		lblExposure.setBackground(ColorPalette.CUSTOM_BLACK);
 		lblExposure.setForeground(ColorPalette.CUSTOM_BLUE);
 
 		areaText = new Text(this, SWT.BORDER);
-		areaText.setBounds(164, 0, 150, 25);
+		areaText.setBounds(189, 0, 196, 35);
 
 		powerLossCoefficientText = new Text(this, SWT.BORDER);
-		powerLossCoefficientText.setBounds(164, 40, 150, 25);
+		powerLossCoefficientText.setBounds(189, 40, 196, 35);
 
 		solarPowerEfficienyText = new Text(this, SWT.BORDER);
-		solarPowerEfficienyText.setBounds(164, 80, 150, 25);
+		solarPowerEfficienyText.setBounds(189, 80, 196, 35);
 
 		exposureText = new Text(this, SWT.BORDER);
-		exposureText.setBounds(164, 120, 150, 25);
+		exposureText.setBounds(189, 120, 196, 35);
 
 		textBoxes = new Text[] { areaText, powerLossCoefficientText, solarPowerEfficienyText, exposureText };
 	}
