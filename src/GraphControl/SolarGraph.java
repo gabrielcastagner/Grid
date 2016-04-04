c4d4499436612cb86bf27273d52c4ec2d7395f88
Riley McGee <mcgeer@mcmaster.ca>
2016-04-04 17:33:55 -0400

    Preparing merge

--- src/GraphControl/SolarGraph.java
+++ src/GraphControl/SolarGraph.java
@@ -13,7 +13,6 @@ public class SolarGraph {
 	final double topLat = 89,topLon =179,bottomLat = -90,bottomLon = -180;
 	private int V; 
 	private int E;
-	//private HashMap<> solarlist;
 	private HashMap<Location, SolarDataNode> solar;
 	private Set<Location> locations;
 	private HashMap<Location ,ArrayList<Location>> adj;
@@ -22,7 +21,7 @@ public class SolarGraph {
 		setupGraph();
 		this.V = locations.size();
 		this.E = 0;
-		setEdges();
+		//setEdges();
 	}
 	
 	public void setupGraph(){
@@ -43,32 +42,60 @@ public class SolarGraph {
 	public int getEdge(){
 		return E;
 	}
-	private void addEdge(Location l1,Location l2 ){
-		if(adj.get(l1).contains(l2))
-			return;
-		adj.get(l1).add(l2);
-		E++;
-	}
-
-	private void findAdjEdge(Location l){
-			//iteration of adding edges based on the position
-
-			for(Location temp : locations){
-				if(l.adjacent(temp)){
-					//System.out.println(l.toString() + "     " + temp.toString() );
-					addEdge(l, temp);
-				}
-			}
-	}
-	/**
-	 * Set all adjacent edges for every node in the Graph
-	 */
-	public void setEdges(){
-		for(Location location: locations){
-			findAdjEdge(location);
+//	public ArrayList<Location> getAdjEdges(Location l){
+//		ArrayList<Location> printlist = new ArrayList<Location>();
+//		for(int i = 0;i<adj.get(l).size()-1;i++){
+//			printlist.add(adj.get(l).get(i));
+//		}
+//		
+//		return printlist;
+//	}
+//	private void addEdge(Location l1,Location l2 ){
+//		if(adj.get(l1).contains(l2))
+//			return;
+//		adj.get(l1).add(l2);
+//		E++;
+//	}
+//
+//	private void findAdjEdge(Location l){
+//			//iteration of adding edges based on the position
+//
+//			ééfor(Location temp : locations){
+//				if(l.adjacent(temp)){
+//					addEdge(l, temp);
+//				}
+//			}
+//	}
+//	/**
+//	 * Set all adjacent edges for every node in the Graph
+//	 */
+//	public void setEdges(){
+//		for(Location location: locations){
+//			findAdjEdge(location);
+//		}
+//	}
+	
+	public ArrayList<Location> BFS(Location l, double d){
+		ArrayList<Location> printlist = new ArrayList<Location>();
+		ArrayList<Location> queue = new ArrayList<Location>();
+		ArrayList<Location> visited = new ArrayList<Location>();
+		
+		queue.add(l);
+		
+		while(!queue.isEmpty()){
+			Location current = queue.remove(0);
+			visited.add(current);
+			
+			
+			
+			
 		}
+		
+		
+		
+		return printlist;
 	}
-
+	
 	public static void main(String args[]){
 		System.out.println("Start");
 		SolarGraph sg = new SolarGraph();
